fa4051426c5f9ce32f61454bbb5d6c578f6e4151
nisa2109 <vivoa5116@gmail.com>
2026-05-20 13:13:57 +0700

    chore: add google search console html verification file

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="google-site-verification" content="kqEhVNvUWQCklGA-taVSa5Md5SlUawyWmC3lbeEu_Lo" />
         <title>{{ $title ?? config('app.name') }}</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="google-site-verification" content="kqEhVNvUWQCklGA-taVSa5Md5SlUawyWmC3lbeEu_Lo" />
 
         <title>Laravel</title>
 
